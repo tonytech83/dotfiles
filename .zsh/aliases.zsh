2638a76418d17104c2bfecd8e8cca095e1b1a6de
tonytech83 <a.petrov.83@gmail.com>
2025-12-10 15:14:12 +0200

    update .zshrc and .zsh/aliases.zsh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -7,6 +7,9 @@ alias tf="terraform"
 alias a="ansible"
 alias ap="ansible-playbook"
 
-alias ls='lsd -a --group-directories-first'
-alias la='lsd -Al --group-directories-first --color auto'
+#alias ls='lsd -a --group-directories-first'
+#alias la='lsd -Al --group-directories-first --color auto'
 alias c='clear'
+
+alias ls='eza -a --group-directories-first --icons=always --color=always'
+alias la='eza -al -h --mounts -g --group-directories-first --icons=always --color=always'
