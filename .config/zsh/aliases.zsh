59e08fc794148ed8f57d7bd3e32fbbf5a65ca92b
Anton Petrov <a.petrov.83@gmail.com>
2026-01-08 10:19:11 +0200

    add aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,15 +1,28 @@
 ##############################################
 ## Aliases
 ##############################################
-alias k="kubectl"
-alias h="helm"
-alias tf="terraform"
-alias a="ansible"
-alias ap="ansible-playbook"
+alias k='kubectl'
+alias h='helm'
+alias tf='terraform'
+alias a='ansible'
+alias ap='ansible-playbook'
 
-#alias ls='lsd -a --group-directories-first'
-#alias la='lsd -Al --group-directories-first --color auto'
+# Navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+
 alias c='clear'
 
+# Changing `ls` to `eza`
 alias ls='eza -a --group-directories-first --icons=always --color=always'
-alias la='eza -al -h --mounts -g --group-directories-first --icons=always --color=always'
+alias la='eza -al -h --mounts -g --group-directories-first --icons=always --color=always --git-repos-no-status'
+alias lt='eza -aT --group-directories-first --icons=always --color=always --level 2' # tree listing
+
+# Vim
+alias vim='nvim'
+
+# System Tools
+alias grep='grep --color=auto'
+alias psa='ps auxf'
+alias psmem='ps auxf | sort -nr -k 4'
+alias pscpu='ps auxf | sort -nr -k 3'
