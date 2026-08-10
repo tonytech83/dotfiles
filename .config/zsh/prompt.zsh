28e238c2d90e1129cab1410f3fa15d7b6a49b154
Anton Petrov <a.petrov.83@gmail.com>
2026-08-10 11:59:58 +0300

    update on-my-posh profiles

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -6,7 +6,7 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 FUNCNEST=100
 
 if command -v oh-my-posh >/dev/null; then
-    eval "$(oh-my-posh init zsh --config ~/dotfiles/.config/oh-my-posh/code.toml)"
+    eval "$(oh-my-posh init zsh --config ~/dotfiles/.config/oh-my-posh/zen.toml)"
 else
     echo "Warning: Oh-My-Posh not found in $OMP_HOME" >&2
 fi
